7ec8048b04281fde90e9dcb4ae440256ff33e475
Khakiss <75739099+jyhan0625@users.noreply.github.com>
2023-11-28 00:09:15 +0900

    Title: 선두를 지켜라 3, Time: 14ms, Memory: 5MB, Status: Passed - CodeTree

---
+++ 231128/선두를 지켜라 3/keep-the-lead-3.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+#define MAXR 1000000
+
+
+/*
+
+a가 선두 : 1
+b가 선두 : 2
+a, b 동시 : 3
+
+*/
+
+int a[MAXR + 1], b[MAXR + 1], c[MAXR + 1];
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m;
+    cin >> n >> m;
+
+
+    int time = 1;
+
+    for(int i=0; i<n; i++) {
+
+        int v, t;
+        cin >> v >> t;
+
+        for(int i=0; i<t; i++) {
+
+            a[time] = a[time - 1] + v;
+            time++;
+
+        }
+
+    }
+
+    time = 1;
+
+    for(int i=0; i<m; i++) {
+
+        int v, t;
+        cin >> v >> t;
+
+        for(int i=0; i<t; i++) {
+
+            b[time] = b[time - 1] + v;
+            time++;
+
+        }
+
+    }
+
+    for(int i=0; i<time; i++) {
+
+        if (a[i] > b[i]) c[i] = 1;
+        else if (a[i] == b[i]) c[i] = 3;
+        else c[i] = 2;
+
+    }
+
+
+    int cnt = 0;
+
+    for(int i=0; i<time; i++) {
+
+        if (i != 0 && c[i] != c[i - 1]) cnt++;
+
+        //cout << "\t" << i  << ") " << c[i] << endl;
+
+    }
+
+    cout << cnt;
+
+
+    return 0;
+}
