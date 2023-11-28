54dbc8eb4a6d722ec196f63215cdd2f0f6b02acd
Khakiss <75739099+jyhan0625@users.noreply.github.com>
2023-11-29 00:33:31 +0900

    Title: 거울에 레이저 쏘기 2, Time: 1092ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 231129/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+using namespace std;
+#define MAXN 1000
+
+/*
+
+[/]
+오른쪽 -> 위쪽 [반시계]
+위쪽 -> 오른쪽 [시계]
+왼쪽 -> 아래쪽 [반시계]
+아래쪽 -> 왼쪽 [시계]
+
+[\]
+오른쪽 -> 아래쪽 [시계]
+위쪽 -> 왼쪽 [반시계]
+왼쪽 -> 위쪽 [시계]
+아래쪽 -> 오른쪽 [반시계]
+
+*/
+
+
+
+char set[MAXN][MAXN] = {'\0'};
+
+int dx[4] = {0,1,0,-1}, dy[4] = {1,0,-1,0};
+
+int firstrot(int rot) {
+
+    if (rot == 0) return (rot + 3) % 4;
+    else if (rot == 1) return (rot + 1) % 4;
+    else if (rot == 2) return (rot + 3) % 4;
+    else if (rot == 3) return (rot + 1) % 4;
+
+}
+
+int secondrot(int rot) {
+
+    if (rot == 0) return (rot + 1) % 4;
+    else if (rot == 1) return (rot + 3) % 4;
+    else if (rot == 2) return (rot + 1) % 4;
+    else if (rot == 3) return (rot + 3) % 4;
+
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+    cin >> n;
+
+    for(int i = 1; i<=n; i++) {
+
+        for(int j=1; j<=n; j++) {
+
+            cin >> set[i][j];
+
+        }
+
+    }
+
+    int start;
+    cin >> start;
+
+    int x =0, y =0, rot = 0;
+ 
+    int tstart = start;
+
+    while(tstart>0) {
+
+        int tx = x + dx[rot];
+        int ty = y + dy[rot];
+
+        if (tx >= 0 && tx<=n && ty >=0 && ty<=n) {
+
+            x = tx;
+            y = ty;
+            tstart--;
+
+        }
+
+        else {
+
+            x = tx;
+            y = ty;
+            rot = (rot + 1) % 4;
+
+        }
+
+    }
+
+    rot = ((start - 1) / n + 1) % 4;
+
+
+
+    int cnt = 0;
+    bool trigger = true;
+
+    while(trigger) {
+
+        x += dx[rot];
+        y += dy[rot];
+
+        if (set[x][y] == '\\') {
+
+            cnt ++;
+            rot = secondrot(rot);
+
+        } 
+
+        else if (set[x][y] == '/') {
+
+            cnt ++;
+            rot = firstrot(rot);
+
+        }
+
+        else trigger = false;
+
+
+
+
+        
+
+    }
+
+    cout << cnt;
+
+    return 0;
+}
