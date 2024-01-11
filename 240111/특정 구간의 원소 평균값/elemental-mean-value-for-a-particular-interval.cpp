883d462479c8f13157062838197479f865b36458
Khakiss <75739099+jyhan0625@users.noreply.github.com>
2024-01-11 18:42:42 +0900

    Title: 특정 구간의 원소 평균값, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240111/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int set[100];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    int ans = 0;
+    for(int i=0; i<n; i++) cin >> set[i];
+
+    for(int i=0; i<n; i++) {
+
+        for(int j=i; j<n; j++) {
+
+            float sum = 0;
+            for(int k=i; k<=j; k++) {
+
+                sum += set[k];
+
+            }
+
+            sum /= (j - i + 1);
+
+            for(int k=0; k<n; k++) {
+
+                if (sum == set[k]) {
+
+                    ans++;
+                    break;
+
+                }
+
+            } 
+
+
+        }
+
+    }
+
+    cout << ans;
+
+
+    return 0;
+}
