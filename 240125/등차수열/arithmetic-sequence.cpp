8722e7423d19bb08f6f06bea4f4efd52277b4da2
Khakiss <75739099+jyhan0625@users.noreply.github.com>
2024-01-25 20:24:50 +0900

    Title: 등차수열, Time: 13ms, Memory: 0MB, Status: Passed - CodeTree

--- 240125/등차수열/arithmetic-sequence.cpp
+++ 240125/등차수열/arithmetic-sequence.cpp
@@ -9,26 +9,25 @@ int main() {
 
     int n;
     cin >> n;
-    int maxnum = 0;
-    for(int i=0; i<n; i++) {
-
-        cin >> set[i];
-        maxnum = max(maxnum, set[i]);
-
-    }
+    for(int i=0; i<n; i++) cin >> set[i];
 
     int ans = 0;
 
-    for(int i=1; i<=maxnum; i++) { // 순차적으로 돌 i
+    for(int i = 1; i<=100; i++) { // 등차중항 설정 : i
 
-        for(int j = 0; j<n; j++) { // 공차의 대상 설정
-            
-            int dist = i - set[j];
-            if (dist <= 0) continue;
+        int cnt = 0;
+        for(int j=0; j<n; j++) {
 
-            for(int k=0; k<n; k++) if (set[k] - i == dist) ans++; // 같은 공차를 가지고 있는 애를 찾는다.
+            for(int k=j+1; k<n; k++) {
 
-        }    
+                if (set[j] + set[k] == i * 2) cnt++;
+
+            }
+
+        }
+
+        ans = max(ans, cnt);
+
     }
 
     cout << ans;
