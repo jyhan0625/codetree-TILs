020647e4f9c4a6fbf7f6755465bb49a7676b310c
Khakiss <75739099+jyhan0625@users.noreply.github.com>
2024-02-19 23:25:34 +0900

    Title: 훌륭한 점프, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240219/훌륭한 점프/great-jump.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int set[101];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, k;
+    cin >> n >> k;
+    int maxnum = 0;
+    for(int i=1; i<=n; i++) {
+        cin >> set[i];
+        maxnum = max(set[i], maxnum);
+    }
+
+
+    int ans = INT_MAX;
+
+    for(int a=max(set[1], set[n]); a<=maxnum; a++) {
+
+        int check[n+1] = {0};
+        int index = 1;
+
+        for(int i=1; i<=n; i++) {
+
+            if (set[i] <= a) {
+
+                check[index++] = i;
+
+            }
+
+        }
+
+        //cout << endl;
+
+        //cout << "a : " << a << endl;
+
+        bool trig = true;
+        for(int i=1; i<index-1; i++) {
+
+            //cout << check[i] << " ";
+
+            if (abs(check[i] - check[i+1]) > k) {
+                trig = false;
+                break;
+            }
+
+        }
+
+        if (trig) {
+
+            ans = min(ans, a);
+
+        }
+
+
+    }
+
+
+
+    cout << ans;
+
+    return 0;
+}
