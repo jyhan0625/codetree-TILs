754b75e939f910b433a2aae84f80d6be6b42dde1
Khakiss <75739099+jyhan0625@users.noreply.github.com>
2024-02-28 01:05:45 +0900

    Title: 구역 청소, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240228/구역 청소/district-cleaning.cpp
+++ 240228/구역 청소/district-cleaning.cpp
@@ -7,9 +7,11 @@ int main() {
     cin >> ax >> ay >> bx >> by;
 
     if (ax <= bx && ay <= bx) cout << (ay - ax) + (by - bx);
-    else if (ax <= bx && ay > bx) cout << (ay - ax) + (by - bx) - (ay - bx);
-    else if (bx <= ax && by <= ax) cout << (ay - ax) + (by - bx);
-    else if (bx <= ax && by > ax) cout << (ay - ax) + (by - bx) - (by - ax);
+    if (ax <= bx && ay > bx) cout << (ay - ax) + (by - bx) - (ay - bx);
+    if (bx <= ax && by <= ax) cout << (ay - ax) + (by - bx);
+    if (bx <= ax && by > ax) cout << (ay - ax) + (by - bx) - (by - ax);
+    if (ax <= bx && by <= ay) cout << (ay - ax);
+    if (bx <= ax && ay <= by) cout << (by - bx);
 
     return 0;
 }
