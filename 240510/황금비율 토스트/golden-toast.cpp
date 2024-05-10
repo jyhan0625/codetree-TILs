faa1323a886a680afe7130f5b9dee3eaba4553ab
Khakiss <75739099+jyhan0625@users.noreply.github.com>
2024-05-10 09:48:56 +0900

    Title: 황금비율 토스트, Time: 17ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240510/황금비율 토스트/golden-toast.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <list>
+#include <string>
+
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    list<char>bread;
+    list<char>::iterator it;
+
+    int n, m;
+    cin >> n >> m;
+
+    for(int i=0; i<n; i++) {
+
+        char c;
+        cin >> c;
+        bread.push_back(c);
+
+    }
+
+    it = bread.end();
+
+    for(int i=0; i<m; i++) {
+
+        char order;
+        cin >> order;
+
+        if(order == 'L') {
+
+            if(it == bread.begin()) continue;
+            else it--;
+
+        }
+
+        else if(order == 'R') {
+
+            if(++it == bread.end()) { 
+                it--;
+            }
+            
+            else it++;
+
+        }
+
+        else if(order == 'P') {
+
+            char input;
+            cin >> input;
+            bread.insert(it, input);
+            it++;
+
+        }
+
+        else if(order == 'D') {
+
+            it = bread.erase(it);
+
+        }
+
+    }
+
+    for(it=bread.begin(); it != bread.end(); it++) {
+
+        cout << *it;
+
+    }
+
+
+    return 0;
+}
