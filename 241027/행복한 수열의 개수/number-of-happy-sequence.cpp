e29d76376a41e6edc19e9c772efaaf873122e325
Khakiss <75739099+jyhan0625@users.noreply.github.com>
2024-10-27 04:27:47 +0900

    Title: 행복한 수열의 개수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241027/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int spc[100][100];
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m;
+    cin >> n >> m;
+
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n; j++) cin >> spc[i][j];
+    }
+
+    int cnt = 0;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n-1; j++) {
+            if(spc[i][j] == spc[i][j+1]) {
+                cnt++;
+                break;
+            }
+        }
+    }
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n-1; j++) {
+            if(spc[j][i] == spc[j+1][i]) {
+                cnt++;
+                break;
+            }
+        }
+    }
+
+    cout << cnt;
+
+    
+    return 0;
+}
